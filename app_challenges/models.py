d75c0914d682c456288cf255a7ca4f94484ae1f3
Jan <jan_rock@hotmail.de>
2018-09-17 17:38:09 +0200

    Add additional fields to Challenge Model

--- app_challenges/models.py
+++ app_challenges/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+from django.utils.crypto import get_random_string
+
+import datetime
 
 class Challenge(models.Model):
 
-  # First Name and Last Name do not cover name patterns
-  # around the globe.
-  city = models.CharField(("City"), blank=True, max_length=150)
+  YEAR_CHOICES = [(r,r) for r in range(2018, datetime.date.today().year+2)]
 
-  # year
-  # context
-  # start_date
-  # end_date
+  context = models.CharField(('Kontext (Schulen- / Kommunenname)'), blank=True, max_length=150)
+  city = models.CharField(('Stadt'), blank=True, max_length=150)
+  year = models.IntegerField(('Jahr'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+  start_date = models.DateField(('Start-Datum'),null=True, blank=True)
+  end_date = models.DateField(('End-Datum'),null=True, blank=True)
 
   # COMMENT OUT AT NEW DEPLOY (then migrate without creating migrations afterwards uncomment and makemigrations)
-  challenge_code = models.CharField(("Registration Code"),max_length=8, null=True, blank=True)
+  challenge_code = models.CharField(('Challenge Code'),max_length=7, null=True, blank=True, unique=True)
   # COMMENT OUT AT NEW DEPLOY
   
   def __str__(self):
-    return self.city
+    return self.context
+
+  def save(self, *args, **kwargs):
+    # If Challenge doesn't already exist create a new Challenge
+    if self.pk is None:
+
+      self.challenge_code = get_random_string(length=7).upper()
+
+      super(Challenge, self).save(*args, **kwargs)
+
+    # End
+    else:
+      super(Challenge, self).save(*args, **kwargs)
